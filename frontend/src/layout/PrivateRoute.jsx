b1b8f4c9abe3f59ec5a3cdfe8d7c8d812fd2b5a1
Deadlymind <deadlygamex5@gmail.com>
2025-01-03 13:57:51 +0100

    update PrivateRoute with comments

--- frontend/src/layout/PrivateRoute.jsx
+++ frontend/src/layout/PrivateRoute.jsx
@@ -1,15 +1,29 @@
+// ===================== IMPORTS =====================
 import { Navigate } from "react-router-dom";
 import { useAuthStore } from "../store/auth";
 import PropTypes from "prop-types";
 
+// ===================== COMPONENT: PRIVATEROUTE =====================
+/**
+ * PrivateRoute:
+ * Wrap this component around any route (or component) that should be accessible
+ * only if the user is logged in. Otherwise, it redirects to "/login".
+ */
 const PrivateRoute = ({ children }) => {
-    const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
+  // Access the `isLoggedIn` boolean from our auth store (Zustand)
+  const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
 
-    return isLoggedIn ? children : <Navigate to="/login" />;
+  // If the user is logged in, render `children`; otherwise, navigate to /login
+  return isLoggedIn ? children : <Navigate to="/login" />;
 };
 
+// ===================== PROP TYPES =====================
 PrivateRoute.propTypes = {
-    children: PropTypes.element.isRequired,
+  /**
+   * children: the protected React element(s) this route should render 
+   * if the user is authenticated.
+   */
+  children: PropTypes.element.isRequired,
 };
 
 export default PrivateRoute;
